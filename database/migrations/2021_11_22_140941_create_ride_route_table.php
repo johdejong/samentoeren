3c3aa14d745acdb0aeecb7f94f9379c190f3aa8e
Shift <shift@laravelshift.com>
2022-02-10 10:35:30 +0000

    Adopt anonymous migrations

--- database/migrations/2021_11_22_140941_create_ride_route_table.php
+++ database/migrations/2021_11_22_140941_create_ride_route_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRideRouteTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -31,4 +31,4 @@ class CreateRideRouteTable extends Migration
     {
         Schema::dropIfExists('ride_route');
     }
-}
+};
